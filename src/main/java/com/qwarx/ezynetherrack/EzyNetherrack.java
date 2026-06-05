944ec7f02a1536bcb7731224fc451db2fab08494
aliengummybear7323 <oscarinsh15@gmail.com>
2026-06-05 17:30:42 +0100

    Add main mod files

---
+++ src/main/java/com/qwarx/ezynetherrack/EzyNetherrack.java
@@ -0,0 +1,12 @@
+package com.qwarx.ezynetherrack;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EzyNetherrack.MODID)
+public class EzyNetherrack {
+    public static final String MODID = "ezynetherrack";
+
+    public EzyNetherrack() {
+        // No initialization needed for simple Create recipes
+    }
+}
